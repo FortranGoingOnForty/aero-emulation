a7210a55deb19ba5aec5f140d57ef74e97a4b8f2
mfwolffe <wolffemf@dukes.jmu.edu>
2025-07-24 16:25:34 -0400

    document cpu bench

--- src_cpu/benchmark_cpu_complex.f90
+++ src_cpu/benchmark_cpu_complex.f90
@@ -1,4 +1,25 @@
-! benchmark_cpu_complex.f90
+! Benchmarking program for aerosol thermodynamics and coagulation sectional kernels on CPU.
+! 
+! This program tests the performance of aerosol thermodynamics and coagulation sectional kernels
+! using synthetic input data. The number of computational cells can be specified as a command-line
+! argument; otherwise, a default value is used.
+!
+! Inputs:
+!   - Number of cells (optional command-line argument)
+!
+! Outputs:
+!   - Timing information for kernel execution over multiple timesteps
+!
+! Workflow:
+!   1. Parse command-line argument for number of cells.
+!   2. Allocate and initialize arrays with synthetic data.
+!   3. Perform warmup iterations to prime caches and JIT.
+!   4. Measure execution time of kernels over specified timesteps.
+!   5. Output timing results and clean up allocated memory.
+! 
+! Author: mfw
+! Date: 2025.07
+!
 program benchmark_cpu_complex
   use aero_kernel_complex_cpu
   implicit none
@@ -12,7 +33,10 @@ program benchmark_cpu_complex
   integer :: i, step, arg_count
   character(len=32) :: arg_str
   
-  ! Get ncells from command line or use default
+  ! 
+  ! Parse command-line arguments for number of cells.
+  ! If no argument is provided, use default value.
+  !
   arg_count = command_argument_count()
   if (arg_count >= 1) then
     call get_command_argument(1, arg_str)
@@ -30,7 +54,10 @@ program benchmark_cpu_complex
   allocate(aerosol_water(n_mode, ncells))
   allocate(size_dist(n_size_bins, ncells))
   
-  ! Initialize
+  ! 
+  ! Initialize synthetic initial conditions for temperature, pressure, relative humidity,
+  ! aerosol mass, and size distribution arrays.
+  !
   do i = 1, ncells
     temp(i) = 298.15 + 10.0 * sin(real(i)/1000.0)
     pres(i) = 101325.0 * (1.0 - 0.1 * sin(real(i)/2000.0))
@@ -39,13 +66,17 @@ program benchmark_cpu_complex
     size_dist(:,i) = 1.0
   end do
   
-  ! Warmup
+  ! 
+  ! Warmup iterations to prime caches and JIT compilation if applicable.
+  !
   do step = 1, 10
     call aerosol_thermodynamics(temp, pres, rh, aerosol_mass, aerosol_water, ncells)
     call coagulation_sectional(size_dist, temp, pres, dt, ncells)
   end do
   
-  ! Benchmark
+  ! 
+  ! Benchmark timing block: measure execution time over nsteps timesteps.
+  !
   call cpu_time(t_start)
   
   do step = 1, nsteps
@@ -59,7 +90,9 @@ program benchmark_cpu_complex
   print *, "Time per timestep:", (t_end - t_start) / real(nsteps), "seconds"
   print *, "Cells/second:", real(ncells * nsteps) / (t_end - t_start)
   
-  ! Cleanup
+  ! 
+  ! Cleanup allocated arrays to free memory.
+  !
   deallocate(temp, pres, rh, aerosol_mass, aerosol_water, size_dist)
   
 end program benchmark_cpu_complex
